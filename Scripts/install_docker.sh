0c2d22070346a4262f1b53b8feb0e70a2303654e
Gabriel Borgogno <geduardae@gmail.com>
2024-04-22 00:39:51 -0300

    Ajustado os parametros de banco

--- Scripts/install_docker.sh
+++ Scripts/install_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Instalação do Docker e o Docker Compose
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo yum install -y docker-compose
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Cria o grupo docker
 sudo groupadd docker
